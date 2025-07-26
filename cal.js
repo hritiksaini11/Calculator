f8ae833b41df7209d903de14a49596faafe059ea
hritiksaini11 <sainihritik8532@gmail.com>
2025-07-26 16:37:48 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,22 @@
+const display = document.getElementById('display');
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    const result = eval(display.value);
+    display.value = result;
+  } catch (e) {
+    display.value = 'Error';
+  }
+}
